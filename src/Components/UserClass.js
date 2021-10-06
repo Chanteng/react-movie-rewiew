e90ebc134125c241a8782417aa121139df70bb2d
Chanteng <yrnchanteng@gmail.com>
2021-10-06 04:03:34 -0700

    added links

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -4,31 +4,51 @@ import axios from 'axios';
 class UserClass extends React.Component {
     constructor() {
         super();
-        this.state = { movies: []};
+        this.state = { movies: [] };
     }
 
-    async getMovies() {
-        const response = await axios.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=QGbVVAlLd6e8aBAuMHUCKSjByeRiAuVL");
-        this.setState ({movies: response.data})
+    // async getMovies() {
+    //     const response = await axios.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=QGbVVAlLd6e8aBAuMHUCKSjByeRiAuVL");
+    //     this.setState ({movies: response.data})
        
+    // }
+
+    async getMovies() {
+      fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=QGbVVAlLd6e8aBAuMHUCKSjByeRiAuVL`)
+      .then((Response) => {
+        return Response.json()
+      })
+      .then((reviews)=> {
+        console.log(reviews.results);
+        return(
+          this.setState({movies: reviews.results})
+        )
+      })
+      
     }
 
     componentDidMount() {
         this.getMovies();
       }
+
+      componentWillUnmount() {
+        this.setState({movies: []})
+      }
+
+
       render() {
         return (
           <>
             {
-              this.state.movies.map((movie) => {
+              this.state.movies.map((movie, index) => {
   
                 return (
-                  <>
-                     <h1 key={movie}>{"display_title"}</h1>
-                     <h1 key={movie}>{"byline"}</h1>
-                     <h1 key={movie}>{"critics_pick"}</h1>
-                    
-                  </>
+                  
+                  <div key={index} className="info">
+                    <h2>{movie.byline}</h2>
+                    <p>{movie.display_title}</p>
+                    <p>{movie.headline}</p>
+                  </div>
                 );
               }
             )}                
